1d24718fdc395454ef32022adf6577c3c41bafe4
nicolanzi1 <nicolanzi@gmail.com>
2020-09-16 00:08:51 +0100

    Wrote an interactive Tabs Widget

--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import Clock from './clock';
+import Tabs from './tabs';
+
+const panes = [
+    {title: 'one', content: 'Hi there sport'},
+    {title: 'two', content: 'Hola amigo'},
+    {title: 'tres', content: 'Ciao amico'}
+];
 
 function Root() {
     return(
         <div>
             <Clock />
+            <div className='interactive'>
+                <Tabs panes={panes} />
+            </div>
         </div>
     )
 }
